d2e9533dddb602e126f14e75c1e732fa48216c46
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-10-25 07:29:26 +0200

    feat(mappings): add important key maps

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -25,3 +25,22 @@
 	(Psalms 37:23 KJV)
 
 --]]
+
+
+--[[ WhichKey Groups ]]--
+
+require("which-key").add({
+	{ "<leader>b", group = "[B]uffers", expand = function() return require("which-key.extras").expand.buf() end },
+	{ "<leader>f", group = "[F]iles" },
+	{ "<leader>w", group = "[W]indows",  proxy = "<c-w>" },
+})
+
+--[[ Buffers ]]--
+
+vim.keymap.set('n', '<leader>bn', ':bnext<enter>', { desc = '[B]uffer [N]ext'     })
+vim.keymap.set('n', '<leader>bp', ':bprev<enter>', { desc = '[B]uffer [P]revious' })
+vim.keymap.set('n', '<leader>bd', ':bdel<enter>',  { desc = '[B]uffer [D]elete'   })
+
+--[[ Files ]]--
+
+vim.keymap.set('n', '<leader>fe', ':Triptych<enter>', { desc = '[F]ile [E]xplorer' })
