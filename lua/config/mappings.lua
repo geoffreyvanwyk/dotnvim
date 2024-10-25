5db6e473d850f4ccc42825fb4c1f85af9f024c88
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-10-25 16:59:55 +0200

    feat(editing): install plugin for splitting and joing code blocks

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -35,20 +35,68 @@ require("which-key").add({
 	{ 
         "<leader>b", 
         group = "[B]uffers", 
-        expand = function() return require("which-key.extras").expand.buf() end 
+        expand = function()
+            return require("which-key.extras").expand.buf()
+        end 
     },
+	{ "<leader>e", group = "[E]diting" },
 	{ "<leader>f", group = "[F]iles" },
 	{ "<leader>w", group = "[W]indows", proxy = "<c-w>" },
 })
 
 ---- BUFFERS ------------------------------------------------------------------
 
-vim.keymap.set('n', '<leader>bn', ':bfirst<enter>',    { desc = '[B]uffer [F]irst'    })
-vim.keymap.set('n', '<leader>bn', ':blast<enter>',     { desc = '[B]uffer [L]ast'     })
-vim.keymap.set('n', '<leader>bn', ':bnext<enter>',     { desc = '[B]uffer [N]ext'     })
-vim.keymap.set('n', '<leader>bp', ':bprevious<enter>', { desc = '[B]uffer [P]revious' })
-vim.keymap.set('n', '<leader>bd', ':bdelete<enter>',   { desc = '[B]uffer [D]elete'   })
+vim.keymap.set(
+    'n',
+    '<leader>bn',
+    ':bfirst<enter>',
+    { desc = '[B]uffer [F]irst' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>bn',
+    ':blast<enter>',
+    { desc = '[B]uffer [L]ast' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>bn',
+    ':bnext<enter>',
+    { desc = '[B]uffer [N]ext' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>bp',
+    ':bprevious<enter>',
+    { desc = '[B]uffer [P]revious' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>bd',
+    ':bdelete<enter>',
+    { desc = '[B]uffer [D]elete' }
+)
+
+---- EDITING ------------------------------------------------------------------
+
+vim.keymap.set(
+    'n',
+    '<leader>es',
+    ':TSJSplit<enter>',
+    { desc = '[E]dit [S]plit Code Block' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>ej',
+    ':TSJJoin<enter>',
+    { desc = '[E]dit [J]oin Code Block' }
+)
 
 ---- FILES --------------------------------------------------------------------
 
-vim.keymap.set('n', '<leader>fe', ':Triptych<enter>', { desc = '[F]ile [E]xplorer' })
+vim.keymap.set(
+    'n',
+    '<leader>fe',
+    ':Triptych<enter>',
+    { desc = '[F]ile [E]xplorer' }
+)
---
+++ lua/plugins/editing.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        --[[
+                Neovim plugin for splitting/joining blocks of code like arrays,
+                hashes, statements, objects, dictionaries, etc.
+        --]]
+        'Wansmer/treesj',
+    }
+}
