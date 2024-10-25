b962c299c30556975888e62e5d006c58f1fcabfc
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-10-25 07:49:35 +0200

    feat(ansible): install plugin for Ansible

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -48,6 +48,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { import = "languages" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
