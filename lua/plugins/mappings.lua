c1c45fd46377137bb781d4f489ea65284d17510f
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-10-26 17:49:28 +0200

    refactor: decorate mappings scripts

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -1,19 +1,48 @@
+--[[
+
+
+
+                                                ███                             
+                                               ░░░                              
+ █████████████    ██████   ████████  ████████  ████  ████████    ███████  █████ 
+░░███░░███░░███  ░░░░░███ ░░███░░███░░███░░███░░███ ░░███░░███  ███░░███ ███░░  
+ ░███ ░███ ░███   ███████  ░███ ░███ ░███ ░███ ░███  ░███ ░███ ░███ ░███░░█████ 
+ ░███ ░███ ░███  ███░░███  ░███ ░███ ░███ ░███ ░███  ░███ ░███ ░███ ░███ ░░░░███
+ █████░███ █████░░████████ ░███████  ░███████  █████ ████ █████░░███████ ██████ 
+░░░░░ ░░░ ░░░░░  ░░░░░░░░  ░███░░░   ░███░░░  ░░░░░ ░░░░ ░░░░░  ░░░░░███░░░░░░  
+                           ░███      ░███                       ███ ░███        
+                           █████     █████                     ░░██████         
+                          ░░░░░     ░░░░░                       ░░░░░░          
+
+
+
+	Key mapping is used to change the meaning of typed keys.
+
+--]]
+
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    preset = "modern",
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local",
+    --[[
+        helps you remember your Neovim keymaps, by showing available keybindings in
+        a popup as you type.
+    --]]
+    
+    "folke/which-key.nvim",
+
+    event = "VeryLazy",
+
+    opts = {
+        preset = "modern",
     },
-  },
+
+    keys = {
+        {
+            "<leader>?",
+
+            function()
+                require("which-key").show({ global = false })
+            end,
+
+            desc = "Buffer Local",
+        },
+    },
 }
