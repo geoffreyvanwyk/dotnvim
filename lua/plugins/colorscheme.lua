a55c493b1fbda8d1f4c14fbdc5834495419f1814
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-10-26 10:48:43 +0200

    refactor: decorate colorscheme script

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,47 @@
+--[[
+
+
+
+                    ████                                       
+                  ░░███                                       
+  ██████   ██████  ░███   ██████  ████████                    
+ ███░░███ ███░░███ ░███  ███░░███░░███░░███                   
+░███ ░░░ ░███ ░███ ░███ ░███ ░███ ░███ ░░░                    
+░███  ███░███ ░███ ░███ ░███ ░███ ░███                        
+░░██████ ░░██████  █████░░██████  █████                       
+ ░░░░░░   ░░░░░░  ░░░░░  ░░░░░░  ░░░░░                        
+                                                              
+                  █████                                       
+                 ░░███                                        
+  █████   ██████  ░███████    ██████  █████████████    ██████ 
+ ███░░   ███░░███ ░███░░███  ███░░███░░███░░███░░███  ███░░███
+░░█████ ░███ ░░░  ░███ ░███ ░███████  ░███ ░███ ░███ ░███████ 
+ ░░░░███░███  ███ ░███ ░███ ░███░░░   ░███ ░███ ░███ ░███░░░  
+ ██████ ░░██████  ████ █████░░██████  █████░███ █████░░██████ 
+░░░░░░   ░░░░░░  ░░░░ ░░░░░  ░░░░░░  ░░░░░ ░░░ ░░░░░  ░░░░░░  
+                                                              
+                                                                                                                                                                      
+                                                                                                         
+--]]
+
+--[[
+
+        And the manna was as coriander seed, and the colour thereof as the
+        colour of bdellium.
+        (Numbers 11:7 KJV)
+
+--]]
+
 return {
+    --[[
+            Gruvbox Material is a modified version of Gruvbox, the contrast is
+            adjusted to be softer in order to protect developers' eyes.  
+    --]]
+    
 	"sainnhe/gruvbox-material",
+
 	priority = 1000,
+
 	init = function()
         vim.g.gruvbox_material_dim_inactive_windows = true
         vim.g.gruvbox_material_enable_bold          = true
