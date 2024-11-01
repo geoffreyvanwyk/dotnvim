94b60594df5d8d6eb9ab35a4f423381088d94cae
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-11-01 13:38:09 +0200

    feat(vc): install and configure git signs

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -59,6 +59,7 @@ require("which-key").add({
 	{ "<leader>c", group = "Code" },
 	{ "<leader>e", group = "Edit" },
 	{ "<leader>f", group = "Files" },
+	{ "<leader>h", group = "Diff Hunks" },
 	{ "<leader>s", group = "Search" },
 	{ "<leader>t", group = "Toggle" },
 	{ "<leader>w", group = "Windows", proxy = "<c-w>" },
@@ -112,6 +113,39 @@ M.leader_mappings_for_code = function(event)
 	map("<leader>cR", require("telescope.builtin").lsp_references, "References")
 end
 
+-------- DIFF -----------------------------------------------------------------
+
+M.leader_mappings_for_diff_actions = function(bufnr)
+	local gitsigns = require("gitsigns")
+
+	local function map(mode, l, r, opts)
+		opts = opts or {}
+		opts.buffer = bufnr
+		vim.keymap.set(mode, l, r, opts)
+	end
+
+	-- Actions
+	-- visual mode
+	map("v", "<leader>hs", function()
+		gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end, { desc = "stage git hunk" })
+	map("v", "<leader>hr", function()
+		gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end, { desc = "reset git hunk" })
+	-- normal mode
+	map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
+	map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
+	map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
+	map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "git [u]ndo stage hunk" })
+	map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
+	map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
+	map("n", "<leader>hb", gitsigns.blame_line, { desc = "git [b]lame line" })
+	map("n", "<leader>hd", gitsigns.diffthis, { desc = "git [d]iff against index" })
+	map("n", "<leader>hD", function()
+		gitsigns.diffthis("@")
+	end, { desc = "git [D]iff against last commit" })
+end
+
 -------- EDIT -----------------------------------------------------------------
 
 vim.keymap.set("n", "<leader>es", ":TSJSplit<enter>", { desc = "Split Code Block" })
@@ -159,6 +193,41 @@ M.leader_mappings_for_inlay_hints = function(event)
 		buffer = event.buf,
 		desc = "LSP: " .. "Inlay Hints",
 	})
+end
+
+M.leader_mappings_for_diff_toggles = function(bufnr)
+	vim.keymap.set(
+		"n",
+		"<leader>tb",
+		require("gitsigns").toggle_current_line_blame,
+		{ desc = "git show [b]lame line", buffer = bufnr }
+	)
+	vim.keymap.set(
+		"n",
+		"<leader>tD",
+		require("gitsigns").toggle_deleted,
+		{ desc = "git show [D]eleted", buffer = bufnr }
+	)
+end
+
+-------- DIFF -----------------------------------------------------------------
+
+M.mappings_for_diff_navigation = function(bufnr)
+	vim.keymap.set("n", "]c", function()
+		if vim.wo.diff then
+			vim.cmd.normal({ "]c", bang = true })
+		else
+			require("gitsigns").nav_hunk("next")
+		end
+	end, { desc = "Jump to next git [c]hange", buffer = bufnr })
+
+	vim.keymap.set("n", "[c", function()
+		if vim.wo.diff then
+			vim.cmd.normal({ "[c", bang = true })
+		else
+			require("gitsigns").nav_hunk("prev")
+		end
+	end, { desc = "Jump to previous git [c]hange", buffer = bufnr })
 end
 
 ---- SEARCHING & PATTERNS -----------------------------------------------------
